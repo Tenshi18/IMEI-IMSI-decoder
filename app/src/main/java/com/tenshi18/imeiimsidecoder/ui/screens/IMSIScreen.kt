aa422a756f710ae7f2e35af4ca42bbb9824ec744
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-02 17:36:44 +0300

    + заглуша с названием экрана

--- app/src/main/java/com/tenshi18/imeiimsidecoder/ui/screens/IMSIScreen.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/ui/screens/IMSIScreen.kt
@@ -1,9 +1,15 @@
 package com.tenshi18.imeiimsidecoder.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun IMSIScreen (modifier: Modifier = Modifier) {
-
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "IMSI Screen")
+    }
 }
