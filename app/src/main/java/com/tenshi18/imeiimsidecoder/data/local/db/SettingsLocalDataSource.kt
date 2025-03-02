e7ab1b6af48f027982b96b469ae7e0317351c8c9
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-02 23:43:25 +0300

    + хранилище настроек DataStore + настройка "использовать динамические цвета MD3"

---
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/data/local/db/SettingsLocalDataSource.kt
@@ -0,0 +1,43 @@
+package com.tenshi18.imeiimsidecoder.data.local.db
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+private const val SETTINGS_DATASTORE_NAME = "user_settings"
+
+private val Context.settingsDataStore by preferencesDataStore(
+    name = SETTINGS_DATASTORE_NAME
+)
+
+class SettingsLocalDataSource(
+    private val context: Context
+) {
+    // Ключ для настройки "использовать динамические цвета MD3"
+    private val USE_DYNAMIC_COLOURS_KEY = booleanPreferencesKey("use_dynamic_colours")
+
+    val useDynamicColoursFlow: Flow<Boolean> = context.settingsDataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+
+        .map { preferences ->
+            preferences[USE_DYNAMIC_COLOURS_KEY] ?: true
+        }
+
+    suspend fun setDynamicColoursEnabled(value: Boolean) {
+        context.settingsDataStore.edit { preferences ->
+            preferences[USE_DYNAMIC_COLOURS_KEY] = value
+        }
+    }
+}
