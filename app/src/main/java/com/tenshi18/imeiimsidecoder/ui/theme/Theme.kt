03aad976d71e0c277d819a2f5d035c47d1eccad6
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-02 17:34:01 +0300

    + тема Midnight Blue

--- app/src/main/java/com/tenshi18/imeiimsidecoder/ui/theme/Theme.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/ui/theme/Theme.kt
@@ -9,28 +9,61 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF26236B),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFF1C1D54),
+    onPrimaryContainer = Color(0xFFFFFFFF),
+
+    secondary = Color(0xFF150F38),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFF1C1D54),
+    onSecondaryContainer = Color(0xFFFFFFFF),
+
+    tertiary = Color(0xFF151345),
+    onTertiary = Color(0xFFFFFFFF),
+    tertiaryContainer = Color(0xFF1C1D54),
+    onTertiaryContainer = Color(0xFFFFFFFF),
+
+    background = Color(0xFF040622),
+    onBackground = Color(0xFFFFFFFF),
+
+    surface = Color(0xFF040622),
+    onSurface = Color(0xFFFFFFFF),
+
+    surfaceVariant = Color(0xFF151345),
+    onSurfaceVariant = Color(0xFFFFFFFF),
+
+    error = Color(0xFFCF6679),
+    onError = Color(0xFFFFFFFF),
+    errorContainer = Color(0xFF8C1D28),
+    onErrorContainer = Color(0xFFFFFFFF),
+
+    outline = Color(0xFF8A8A8A),
+
+    inverseOnSurface = Color(0xFF151345),
+    inverseSurface = Color(0xFFFFFFFF),
+    inversePrimary = Color(0xFF26236B),
+
+    surfaceTint = Color(0xFF26236B)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Color(0xFF003366),
+    onPrimary = Color(0xFFFFFFFF),
+    secondary = Color(0xFF336699),
+    onSecondary = Color(0xFFFFFFFF),
+    tertiary = Color(0xFF6688CC),
+    onTertiary = Color(0xFFFFFFFF),
+    error = Color(0xFFD00000),
+    onError = Color(0xFFFFFFFF),
+    background = Color(0xFF000022),
+    onBackground = Color(0xFFFFFFFF),
+    surface = Color(0xFF000033),
+    onSurface = Color(0xFFFFFFFF)
 )
 
 @Composable
