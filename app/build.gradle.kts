12b888f3dd1f42d456841bddea8f1dad08b159a3
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-02 19:02:16 +0300

    + Preferences DataStore для сохранения настроек

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.room.common)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,6 +67,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 
     // Navigation Compose для Bottom Navigation Bar
     implementation(libs.androidx.navigation.compose)
