1cb2c95c97d6a2ee8ac3eae7e3690baab43b0a38
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-02 23:31:48 +0300

    + isDynamicColorEnabled, setDynamicColorEnabled в SettingsRepository

--- app/src/main/java/com/tenshi18/imeiimsidecoder/domain/SettingsRepository.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/domain/SettingsRepository.kt
@@ -2,4 +2,7 @@ package com.tenshi18.imeiimsidecoder.domain
 
 interface SettingsRepository {
 
+    suspend fun isDynamicColorEnabled(): Boolean
+    suspend fun setDynamicColorEnabled(value: Boolean)
+
 }
