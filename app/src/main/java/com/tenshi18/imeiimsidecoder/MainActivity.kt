7a354f0cf7dc918a82bde771bca81305e970d3ae
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-04 13:37:44 +0300

    + добавление SettingsViewModel в MainActivity + SettingsLocalDataSource и SettingsRepositoryImpl в MainActivity

--- app/src/main/java/com/tenshi18/imeiimsidecoder/MainActivity.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.tenshi18.imeiimsidecoder.data.local.SettingsLocalDataSource
 import com.tenshi18.imeiimsidecoder.data.repository.SettingsRepositoryImpl
 import com.tenshi18.imeiimsidecoder.presentation.theme.IMEIIMSIDecoderTheme
 import com.tenshi18.imeiimsidecoder.presentation.components.NavigationController
+import com.tenshi18.imeiimsidecoder.presentation.viewmodels.SettingsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +19,23 @@ class MainActivity : ComponentActivity() {
         val localDataSource = SettingsLocalDataSource(applicationContext)
         val settingsRepository = SettingsRepositoryImpl(localDataSource)
 
+        val factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                if (modelClass.isAssignableFrom(SettingsViewModel::class.java)) {
+                    @Suppress("UNCHECKED_CAST")
+                    return SettingsViewModel(settingsRepository) as T
+                }
+                throw IllegalArgumentException("Unknown ViewModel class")
+            }
+        }
+
+        val settingsViewModel = ViewModelProvider(this, factory)[SettingsViewModel::class.java]
+
         enableEdgeToEdge()
         setContent {
             IMEIIMSIDecoderTheme {
 
-                NavigationController()
+                NavigationController(settingsViewModel)
 
             }
         }
