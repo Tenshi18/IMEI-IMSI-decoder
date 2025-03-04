f9ac32d354771108fac083686462a9975a0ac12c
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-04 13:28:04 +0300

    + `SettingsLocalDataSource`, `SettingsRepositoryImpl` в MainActivity

--- app/src/main/java/com/tenshi18/imeiimsidecoder/MainActivity.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.tenshi18.imeiimsidecoder.data.local.SettingsLocalDataSource
+import com.tenshi18.imeiimsidecoder.data.repository.SettingsRepositoryImpl
 import com.tenshi18.imeiimsidecoder.presentation.theme.IMEIIMSIDecoderTheme
 import com.tenshi18.imeiimsidecoder.presentation.components.NavigationController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val localDataSource = SettingsLocalDataSource(applicationContext)
+        val settingsRepository = SettingsRepositoryImpl(localDataSource)
+
         enableEdgeToEdge()
         setContent {
             IMEIIMSIDecoderTheme {
