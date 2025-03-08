8f9ffb099b6275716ff4437a423a3f871343b27f
Tenshi18 <142268664+Tenshi18@users.noreply.github.com>
2025-03-08 15:53:05 +0300

    + Переход к SwitchPreference # - Добавлен компонент SwitchPreference для настроек;   - замена Row на Column

--- app/src/main/java/com/tenshi18/imeiimsidecoder/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/presentation/settings/SettingsScreen.kt
@@ -1,18 +1,12 @@
 package com.tenshi18.imeiimsidecoder.presentation.settings
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 
 @Composable
 fun SettingsScreen(settingsViewModel : SettingsViewModel) {
@@ -20,16 +14,16 @@ fun SettingsScreen(settingsViewModel : SettingsViewModel) {
     // Подписываемся на StateFlow из ViewModel
     val useDynamicColours by settingsViewModel.useDynamicColoursFlow.collectAsState()
 
-    Row(
-        verticalAlignment = CenterVertically, modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 16.dp)
-    ) {
-        Text(text = "Динамческая тема (Material You)")
-        Spacer(Modifier.width(12.dp))
-        Switch(
+    Column {
+
+        SwitchPreference(
+            modifier = Modifier.fillMaxWidth(),
+            title = { Text("Динамическая тема (Material You)") },
+            description = "Использовать цвета обоев",
             checked = useDynamicColours,
             onCheckedChange = { settingsViewModel.setDynamicColoursEnabled(it) }
         )
+
     }
 }
+
