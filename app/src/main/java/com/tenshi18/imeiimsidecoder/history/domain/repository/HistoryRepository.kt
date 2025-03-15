65fa4ff86a6c669d2efcbfaa1148aabd231cb1af
unknown <142268664+Tenshi18@users.noreply.github.com>
2025-03-15 12:02:34 +0300

    + репозиторий HistoryRepository

--- app/src/main/java/com/tenshi18/imeiimsidecoder/history/domain/repository/HistoryRepository.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/history/domain/repository/HistoryRepository.kt
@@ -1,2 +1,10 @@
 package com.tenshi18.imeiimsidecoder.history.domain.repository
 
+import com.tenshi18.imeiimsidecoder.history.domain.model.HistoryItem
+import kotlinx.coroutines.flow.Flow
+
+interface HistoryRepository {
+    val historyFlow: Flow<List<HistoryItem>>
+    suspend fun addHistoryItem(item: HistoryItem)
+    suspend fun clearHistory()
+}
