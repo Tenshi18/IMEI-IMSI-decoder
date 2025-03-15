52615ad1bb03cd4f8fe3915ccc443ffbd5e7f822
unknown <142268664+Tenshi18@users.noreply.github.com>
2025-03-15 12:31:35 +0300

    + реализация экрана истории с LazyColumn

--- app/src/main/java/com/tenshi18/imeiimsidecoder/history/presentation/screens/HistoryScreen.kt
+++ app/src/main/java/com/tenshi18/imeiimsidecoder/history/presentation/screens/HistoryScreen.kt
@@ -1,15 +1,54 @@
-package com.tenshi18.imeiimsidecoder.ui.screens
+package com.tenshi18.imeiimsidecoder.history.presentation.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.tenshi18.imeiimsidecoder.history.domain.model.HistoryItem
+import com.tenshi18.imeiimsidecoder.history.presentation.viewmodels.HistoryViewModel
+import java.text.DateFormat
+import java.util.Date
 
 @Composable
-fun HistoryScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Экран истории (ещё не реализовано)")
+fun HistoryScreen(historyViewModel: HistoryViewModel) {
+
+    val historyList by historyViewModel.historyFlow.collectAsState()
+
+    Column {
+        Row (modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { historyViewModel.clearHistory() }) {
+                Text("Очистить историю")
+            }
+        }
+
+        Spacer(Modifier.height(8.dp))
+
+        LazyColumn {
+            items(historyList) { item ->
+                HistoryItemRow(item)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun HistoryItemRow(item: HistoryItem) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(text = "Тип: ${item.type}")
+        Text(text = "Значение: ${item.value}")
+        Text(text = "Результат: ${item.decoded}")
+        Text(text = "Время: ${DateFormat.getDateTimeInstance().format(Date(item.timestamp))}")
     }
 }
